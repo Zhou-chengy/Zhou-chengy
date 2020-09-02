dc159ceb36c147b933be4b169e9546585b1fca1a
Zhou-chengy <70565781+Zhou-chengy@users.noreply.github.com>
2020-09-02 19:37:09 +0800

    Add files via upload

---
+++ 猜数游戏1.py
@@ -0,0 +1,46 @@
+# coding: gbk
+from random import randint
+num = randint(1,1000)
+print('猜猜这个数是几?（不想玩了回-1)')
+bingo = False
+count = 0
+
+while bingo == False:
+    count +=1
+    answer = int(input())
+    num1 = 10+num
+    x = randint(1,10)
+    y = randint(1,10)
+    z = randint(1,10)
+
+    if answer<num:
+        print(str(answer) + '太小了!')
+        num = num+x
+        
+    if answer>num:
+        print(str(answer) + '太大了')
+        num = num+y
+   
+    if answer==num1:
+        print(str(answer) + '接近了')
+        num = num+z
+
+    if answer==-1:
+        print(str(num) +  '是正确的答案,游戏失败')
+        bingo = True
+
+    if count==50:
+        print('游戏失败')
+        bingo = True
+        
+    if answer==num:
+        print('答对了,'+ str(answer) +' 是正确的答案')
+        bingo = True
+
+print(count)
+
+                
+
+           
+           
+    
